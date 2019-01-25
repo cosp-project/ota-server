ed53e818e5ce21c7684684470f7f64997c2bc821
ATechnoHazard <amolele@gmail.com>
2019-01-26 03:03:17 +0530

    Add more entry fields
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -10,38 +10,45 @@ router.get('/', (req, res) => {
 
 // Submit route
 router.post('/',
-    [body('device')
-        .isLength({ min: 1})
-        .withMessage('Please enter the device name.'),
-     body('maintainer')
-         .isLength({ min: 1})
-         .withMessage('Please enter the maintainer\'s name')
+    [
+        body('device')
+            .isLength({ min: 1 })
+            .withMessage('Please enter the device name.'),
+        body('maintainer')
+            .isLength({ min: 1 })
+            .withMessage('Please enter the maintainer\'s name'),
+        body('buildDate')
+            .isLength({ min: 1 })
+            .withMessage('Please enter the build date.'),
+        body('changeLog')
+            .isLength({ min: 1 })
+            .withMessage('Please enter the changelog.')
     ],
     async (req, res) => {
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
-        try {
-            await Device.findOne({ device: req.body.device }, async (err, device) => {
-                if (err) console.log(err);
-                await device.set(req.body);
-                await device.save((err) => {
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            try {
+                await Device.findOne({ device: req.body.device }, async (err, device) => {
                     if (err) console.log(err);
-                    res.send('Updated successfully.')
+                    await device.set(req.body);
+                    await device.save((err) => {
+                        if (err) console.log(err);
+                        res.send('Updated successfully.')
+                    });
                 });
+            } catch (e) {
+                const device = new Device(req.body);
+                await device.save();
+            }
+        } else {
+            res.render('form', {
+                title: 'COSP Updates',
+                errors: errors.array(),
+                data: req.body
             });
-        } catch (e) {
-            const device = new Device(req.body);
-            await device.save();
         }
-    } else {
-        res.render('form', {
-            title: 'COSP Updates',
-            errors: errors.array(),
-            data: req.body
-        });
-    }
-    console.log(req.body);
-    res.render('form', { title: 'COSP Updates' });
-});
+        console.log(req.body);
+        res.render('form', { title: 'COSP Updates' });
+    });
 
 module.exports = router;
