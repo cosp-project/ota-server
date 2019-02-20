0ee2cf73a1d34a8b3f56a46682e6bfa02d1d8692
ATechnoHazard <amolele@gmail.com>
2019-02-20 23:09:30 +0530

    Add documentation for public API methods
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

---
+++ docs/api_data.js
@@ -0,0 +1,154 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/checkUpdate",
+    "title": "Check if update exists",
+    "name": "GetCheckUpdate",
+    "group": "Update",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "device",
+            "description": "<p>The user's device</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "date",
+            "description": "<p>The build date in the format &quot;yymmdd&quot;</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Boolean",
+            "optional": false,
+            "field": "update",
+            "description": "<p>Whether an update is available</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "download",
+            "description": "<p>Download URL</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "changeLog",
+            "description": "<p>Device update changelog</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\"update\":true,\"download\":\"https://download.example.com\",\"changeLog\":\"Sample Changelog\"}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "DeviceNotFound",
+            "description": "<p>The device was not found</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 404 Not Found\n{\n    \"error\": \"DeviceNotFound\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "routes/index.js",
+    "groupTitle": "Update"
+  },
+  {
+    "type": "get",
+    "url": "/latestDownload",
+    "title": "Get the latest download link",
+    "name": "GetLatestDownload",
+    "group": "Update",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "device",
+            "description": "<p>The user's device</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "date",
+            "description": "<p>The latest build date</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "download",
+            "description": "<p>Download URL</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\"date\":190129,\"download\":\"https://download.example.com\"}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "DeviceNotFound",
+            "description": "<p>The device was not found</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "HTTP/1.1 404 Not Found\n{\n    \"error\": \"DeviceNotFound\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "routes/index.js",
+    "groupTitle": "Update"
+  }
+] });
