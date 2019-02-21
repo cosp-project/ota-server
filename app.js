933dba3622064c88b64e44ef162e5e0520d49013
ATechnoHazard <amolele@gmail.com>
2019-02-21 12:51:12 +0530

    Add a favicon for the webpage
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Express = require('express');
+const favicon = require('serve-favicon');
 const path = require('path');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
@@ -8,8 +9,10 @@ const app = Express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 app.use(Express.static('public'));
+
 
 module.exports = app;
